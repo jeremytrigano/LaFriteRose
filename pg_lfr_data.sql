3e9075f9f80458b0f872108996a78e667b129bac
jeremytrigano <trigano.jeremy@gmail.com>
2019-08-08 16:26:41 +0200

    ajout 1 centre et 1 vacancier

--- pg_lfr_data.sql
+++ pg_lfr_data.sql
@@ -6,7 +6,12 @@ VALUES ('Opio en Provence', 'Chemin Cambarnier-Nord', '06650', 'Opio','Alpes Mar
 INSERT INTO centre (nom, adresse, code_postal, ville, region, pays) 
 VALUES ('La Palmyre Atlantique', 'Allée du grand large La Palmyre', '17570', 'La Palmyre-Les Mathes','Poitou-Charentes', 'France');
 INSERT INTO centre (nom, adresse, code_postal, ville, region, pays)
-VALUES ('Rêv''hôtel', 'Plage d''Acharavi', '49100', 'Acharavi','Corfu', 'Grèce');
+VALUES ('Rêv''hôtel', 'Plage d''Acharavi', '49100', 'Acharavi', 'Corfu', 'Grèce');
+INSERT INTO centre (nom, adresse, code_postal, ville, region, pays)
+VALUES ('L''idylle Arena', 'Viale Spartivento', '09010', 'Domus De Maria', 'Sardaigne', 'Italie');
+
+INSERT INTO vacancier (nom, prenom, date_de_naissance, statut)
+VALUES ('Annaesky', 'Mario', '06/07/1985', 'VIP');
 
 INSERT INTO animateur (nom,prenom) VALUES ('Noah','MacLittis');
 INSERT INTO animateur (nom,prenom) VALUES ('Pyer','Emann');
